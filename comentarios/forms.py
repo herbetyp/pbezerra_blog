b89ccc73f8881fb86b410fb593c66497c453e6c0
Pbezerra-dev <herbetyp@gmail.com>
2020-05-06 14:51:27 -0300

    modified check recapctha

--- comentarios/forms.py
+++ comentarios/forms.py
@@ -9,12 +9,6 @@ class FormComentario(ModelForm):
         raw_data = self.data
         recaptcha_response = raw_data.get('g-recaptcha-response')
 
-        if not recaptcha_response:
-            self.add_error(
-                'comentario',
-                'Por favor, marque a caixa "Não sou um robô" abaixo.'
-            )
-
         recaptcha_request = requests.post(
             'https://www.google.com/recaptcha/api/siteverify',
             data={
@@ -22,19 +16,20 @@ class FormComentario(ModelForm):
                 'response': recaptcha_response
             }
         )
-
         recaptcha_result = recaptcha_request.json()
+        if not recaptcha_result.get('success'):
+            self.add_error(
+                'comentario',
+                'reCAPTCHA inválido. Tente novamente.'
+            )
 
         cleaned_data = self.cleaned_data
         nome = cleaned_data.get('nome_comentario')
-        email = cleaned_data.get('email_comentario')
-        comentario = cleaned_data.get('comentario')
 
         if len(nome) < 5:
             self.add_error(
                 'nome_comentario',
-                'Nome precisa ter mais de 5 caracteres.'
-            )
+                'Nome precisa ter mais de 5 caracteres.')
 
     class Meta:
         model = Comentario
